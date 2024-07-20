74a9a06e72d24d86db3815dc859fbb3cd3e57438
akansh507 <shivhareakansh507@gmail.com>
2024-07-20 17:27:53 +0530

    PRMEntity.java
    update  private Long id;
    	    private String allComplaints;
    	    private String description;

--- Grievence_internship-2/src/main/java/com/springboot/model/PRMEntity.java
+++ Grievence_internship-2/src/main/java/com/springboot/model/PRMEntity.java
@@ -23,8 +23,9 @@ public class PRMEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String description;
+	    private String allComplaints;
+	    private String description;
+
 
     
 }
